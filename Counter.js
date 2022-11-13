192879fcb341821446c4965fc48cf1c745baddce
Michael Sayapin <michael.sayapin@naturalint.com>
2022-11-13 17:38:09 +0200

    set state and function

--- Counter.js
+++ Counter.js
@@ -13,16 +13,37 @@ class Counter extends Component {
     this.reset = this.reset.bind(this);
   }
 
+  /**
+   * Effectively, we are queuing up state changes, so React will batch them up, figure out the result and then
+   * efficiently make that change.
+   */
   increment() {
-    this.setState({ count: this.state.count + 1 })
+    this.setState({ count: this.state.count + 1 });
+    this.setState({ count: this.state.count + 1 });
+    this.setState({ count: this.state.count + 1 });
   }
 
-  decrement() {
-    this.setState({ count: this.state.count - 1 })
-  }
-
+  /**
+   * When we pass functions to setState(), React plays through each of them
+   */
   reset() {
-    this.setState({ count: 0 })
+    this.setState(() => {
+      return { count: 0};
+    });
+  }
+
+  /**
+   * We can use the props to the component directly from the setState method
+   */
+  decrement() {
+    this.setState((state, props) => {
+      const { max, step } = props;
+      if (state.count >= max) {
+        return;
+      }
+
+      return { count: state.count - step }
+    });
   }
 
   render() {
